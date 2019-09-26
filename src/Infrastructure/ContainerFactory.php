13ec60b5c796a6315529b3290ba4e2fef625597d
Shahrokh <shahorkhnabavi@gmail.com>
2019-09-26 21:11:12 +0200

    [NAPI-6] Setup slim framework

---
+++ src/Infrastructure/ContainerFactory.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types = 1);
+
+namespace SiteApi\Infrastructure;
+
+use League\Container\Container;
+use Psr\Container\ContainerInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class ContainerFactory
+{
+    /**
+     * @return ContainerInterface
+     */
+    public static function create(): ContainerInterface
+    {
+        $container = new Container();
+
+        //$container->delegate(new \Slim\Container());
+        //$container->addServiceProvider(SlimServiceProvider::class);
+        //$container->addServiceProvider(PdoServiceProvider::class);
+
+        return $container;
+    }
+}
