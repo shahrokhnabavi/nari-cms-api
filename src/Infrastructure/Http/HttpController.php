808e5da44b76945e6a80785df1cfd216d240beaf
Shahrokh <shahrokhnabavi@yahoo.com>
2019-10-07 23:50:40 +0200

    Revert "[NAPI-11] Create controller based application"
    This reverts commit 012214762a337608b7588ee8ee5f70b16ca1646c.

--- src/Infrastructure/Http/HttpController.php
+++ src/Infrastructure/Http/HttpController.php
@@ -3,42 +3,15 @@ declare(strict_types = 1);
 
 namespace SiteApi\Infrastructure\Http;
 
-use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 class HttpController
 {
-    /** @var ContainerInterface */
-    private $container;
-
-    /**
-     * @param ContainerInterface $container
-     */
-    public function __construct(ContainerInterface $container)
-    {
-        $this->container = $container;
-    }
-
-    /**
-     * @param ResponseInterface $response
-     * @param $text
-     *
-     * @return ResponseInterface
-     */
-    public function html(ResponseInterface $response, $text): ResponseInterface
+    public function html(ResponseInterface $response, $text)
     {
         $response->getBody()->write($text);
 
         return $response;
-    }
-
-    /**
-     * @param string $name
-     *
-     * @return mixed
-     */
-    public function _get(string $name)
-    {
-        return $this->container->get($name);
     }
 }
