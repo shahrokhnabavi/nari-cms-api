3115a2df6229fc153085cced976a50e865cd1761
Shahrokh <shahorkhnabavi@gmail.com>
2019-10-07 23:46:20 +0200

    [NAPI-11] Create controller based application

--- src/Infrastructure/Http/HttpController.php
+++ src/Infrastructure/Http/HttpController.php
@@ -3,15 +3,42 @@ declare(strict_types = 1);
 
 namespace SiteApi\Infrastructure\Http;
 
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
 
 class HttpController
 {
-    public function html(ResponseInterface $response, $text)
+    /** @var ContainerInterface */
+    private $container;
+
+    /**
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param ResponseInterface $response
+     * @param $text
+     *
+     * @return ResponseInterface
+     */
+    public function html(ResponseInterface $response, $text): ResponseInterface
     {
         $response->getBody()->write($text);
 
         return $response;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return mixed
+     */
+    public function getContainerEntry(string $name)
+    {
+        return $this->container->get($name);
     }
 }
