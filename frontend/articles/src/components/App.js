9107100cc27e417be4b11eef28d9279d00b49a0e
Shahrokh <shahorkhnabavi@gmail.com>
2019-10-19 23:55:01 +0200

    [NAPI-30] Add complex reducer and actions to react app

--- frontend/articles/src/components/App.js
+++ frontend/articles/src/components/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import actions from '../actions';
 
 const App = props => {
+  const value = Math.round(Math.random() * (6-1) + 1);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,18 +13,32 @@ const App = props => {
           My Application
         </p>
         <h4>Age: <span>{props.age}</span></h4>
-        <button onClick={props.onAgeUp}>Add</button>
-        <button onClick={props.onAgeDown}>Subtract</button>
+        <div className="Button-Holder">
+          <button onClick={() => props.onAgeUp(value)}>Add</button>
+          <button onClick={() => props.onAgeDown(value)}>Subtract</button>
+        </div>
+        <div className="History">
+          <ul>
+            {
+              props.history.map(el => {
+                return (
+                  <li onClick={() => props.onRemoveItemHistory(el.key)} key={el.key}>{el.log}</li>
+                )
+              })
+            }
+          </ul>
+        </div>
       </header>
     </div>
   );
 };
 
-const mapStateToProps = state => ({age: state.age});
+const mapStateToProps = state => ({age: state.age, history: state.history});
 
 const mapDispatchToProps = dispatch => ({
-  onAgeUp: () => dispatch(actions.AGE_UP),
-  onAgeDown: () => dispatch(actions.AGE_DOWN)
+  onAgeUp: stepUp => dispatch({type: actions.AGE_UP, value: stepUp}),
+  onAgeDown: stepDow => dispatch({type: actions.AGE_DOWN, value: stepDow}),
+  onRemoveItemHistory: id => dispatch({type: actions.REMOVE_ITEM_HISTORY, key: id})
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
