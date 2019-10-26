fdbfa5a3bee5626eaacb580673b23c327427b9c2
Shahrokh <shahorkhnabavi@gmail.com>
2019-10-26 08:13:56 +0200

    [NAPI-35] add manageable admin menu

--- frontend/articles/src/components/AdminLayout/TopBar/index.js
+++ frontend/articles/src/components/AdminLayout/TopBar/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import { Menu as MenuIcon } from "@material-ui/icons";
 import { AppBar, Toolbar } from "@material-ui/core";
 
-import conditionalCssClass from "../../util/conditionalCssClass";
-import { AdminPanelLayoutActions } from "../../actions";
+import conditionalCssClass from "../../../util/conditionalCssClass";
+import { AdminPanelLayoutActions } from "../../../actions";
+import Icon from '../../shared/Icon';
 
 const TopBar = props => {
   const { classes, isMenuOpen, openMenu } = props;
 
-  console.log(conditionalCssClass(classes.appBar, [isMenuOpen, classes.appBarShift]));
   return (
     <AppBar
       position="fixed"
@@ -25,7 +24,7 @@ const TopBar = props => {
           edge="start"
           className={conditionalCssClass(classes.menuButton, [isMenuOpen, classes.hide])}
         >
-          <MenuIcon />
+          <Icon type="menu" />
         </IconButton>
         <Typography variant="h6" noWrap>
           Persistent drawer
